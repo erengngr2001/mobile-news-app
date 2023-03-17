ca7b9d5a6e630c1e4dff978ec818848fcbcde238
erengngr2001 <76160067+erengngr2001@users.noreply.github.com>
2023-03-18 00:07:45 +0300

    Delete Assignment 1/EREN_GUNGOR directory

--- Assignment 1/EREN_GUNGOR/src/main/java/com/example/demo/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-
-import java.util.List;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface AccountRepository extends MongoRepository<Account, String> {
-	List<Account> findAllById(String accountId);
-}
